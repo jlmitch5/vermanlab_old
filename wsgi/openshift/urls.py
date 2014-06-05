a4dae0bca840ab94cc234b609f63c436f770bcb1
John Mitchell <jmitchel@redhat.com>
2014-06-05 16:30:08 -0400

    using @suhailvs pr, I'm getting 1.6 to work

--- wsgi/openshift/urls.py
+++ wsgi/openshift/urls.py
@@ -6,8 +6,8 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'openshift.views.home', name='home'),
-    # url(r'^openshift/', include('openshift.foo.urls')),
+    url(r'^$', 'views.home', name='home'),
+    # url(r'^openshift/', include('foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
