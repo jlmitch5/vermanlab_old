5949e78625fac512068e8c657f18b40f2072f567
John Mitchell <jmitchel@redhat.com>
2014-06-05 16:04:35 -0400

    entered my credentials in setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from setuptools import setup
 
 setup(
-    name='YourAppName',
+    name='vml',
     version='1.0',
     description='OpenShift App',
-    author='Your Name',
-    author_email='example@example.com',
+    author='John Mitchell',
+    author_email='jmitchel@example.com',
     url='http://www.python.org/sigs/distutils-sig/',
     install_requires=['Django<=1.4'],
 )
