b18c32a29fd2ba7c438b1565f3a3adfbb88768b5
John Mitchell <jmitchel@redhat.com>
2014-06-09 16:53:06 -0400

    schema is complete I believe

--- wsgi/openshift/add_kernel_version_drivers/models.py
+++ wsgi/openshift/add_kernel_version_drivers/models.py
@@ -11,7 +11,6 @@ class KernelVersion(models.Model):
     def __unicode__(self):
         return name
 
-    # 
 
     # TODO: add delete functionality via checking
     # exclusivity of module relations
@@ -29,19 +28,22 @@ class PCIModule(models.Model):
     name = models.CharField(max_length=100, null=True, blank=True)
     version = models.CharField(max_length=100, null=True, blank=True)
     srcversion = models.CharField(max_length=100, null=True, blank=True)
-
+    # we'll give this a many-to-many relationship with the Kernel Version
+    # may be updated with the connector additions (tags) later
+    kernelVersionModuleConnector = models.ManyToManyField('KernelVersion', through='KernelVersionModuleConnector')
+    
     # is this okay, if it's not referencing a foreign key?
     # again, any of these values *can* be null
     class Meta:
         unique_together = ('name', 'version', 'srcversion')
 
-    # we'll give this a many-to-many relationship with the Kernel Version
-    # may be updated with the connector additions (tags) later
-    kernelVersions = models.ManyToManyField(KernelVersion)
-
     # returns the Module name version and source version
     def __unicode__(self):
         return self.name+" "+self.version+" "+self.srcversion
+
+class KernelVersionModuleConnector(models.Model):
+    module = models.ForeignKey(PCIModule)
+    kernel = models.ForeignKey(KernelVersion)
 
 # this is superfluous right now...may come in use with "tags"
 # "tags" will also have to encapsulate alias data
