5fe8f220986032892ba4c694e111f6d8535b70db
John Mitchell <jmitchel@redhat.com>
2014-06-10 13:26:52 -0400

    taking out delete on admin page for now, add later

--- wsgi/openshift/upload_kernel/models.py
+++ wsgi/openshift/upload_kernel/models.py
@@ -5,9 +5,3 @@ from django.dispatch import receiver
 
 class Document(models.Model):
     docfile = models.FileField(upload_to='') #default location to upload: to_be_added
-
-    @receiver(post_delete, sender=Document)
-    def photo_post_delete_handler(sender, **kwargs):
-        document = kwargs['instance']
-        storage, path = document.storage, document.path
-        storage.delete(path)
