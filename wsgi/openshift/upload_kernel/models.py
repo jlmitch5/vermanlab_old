82fc93752894728d63757ba0c9e56101e68e2412
John Mitchell <jmitchel@redhat.com>
2014-06-10 10:57:13 -0400

    trying to allow files to be deleted from django panel

--- wsgi/openshift/upload_kernel/models.py
+++ wsgi/openshift/upload_kernel/models.py
@@ -1,6 +1,13 @@
-# Create your models here.
 # -*- coding: utf-8 -*-
 from django.db import models
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
 
 class Document(models.Model):
     docfile = models.FileField(upload_to='') #default location to upload: to_be_added
+
+    @receiver(post_delete, sender=Document)
+    def photo_post_delete_handler(sender, **kwargs):
+        document = kwargs['instance']
+        storage, path = document.storage, document.path
+        storage.delete(path)
