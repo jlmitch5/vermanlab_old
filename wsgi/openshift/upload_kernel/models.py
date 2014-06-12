73bd8c9fa23a8a0d828ef6c088abc323010dbf9d
John Mitchell <jmitchel@redhat.com>
2014-06-12 13:19:48 -0400

    Fixed file deletion within admin panelw

--- wsgi/openshift/upload_kernel/models.py
+++ wsgi/openshift/upload_kernel/models.py
@@ -10,6 +10,15 @@ class Kernel_Tarball(models.Model):
 	no_path_name = self.docfile.name.split('/')[-1]
 	return no_path_name[:-7]
 
+# Receive the pre_delete signal and delete the file associated with the model instance.
+from django.db.models.signals import pre_delete
+from django.dispatch.dispatcher import receiver
+
+@receiver(pre_delete, sender=Kernel_Tarball)
+def kernel_tarball_delete(sender, instance, **kwargs):
+    # Pass false so FileField doesn't save the model.
+    instance.docfile.delete(False)
+
 class Shell_Script(models.Model):
     docfile = models.FileField(upload_to='scripts')
 
