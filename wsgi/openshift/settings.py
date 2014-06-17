b5261bf9629c353db95279e99a4e8a43da5dd826
John Mitchell <jmitchel@redhat.com>
2014-06-17 18:29:05 -0400

    Added apis for each model

--- wsgi/openshift/settings.py
+++ wsgi/openshift/settings.py
@@ -176,6 +176,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'bootstrap3',
+    'rest_framework',
     'upload_kernel',
     'schema_kernel',
 )
@@ -201,4 +202,17 @@ LOGGING = {
             'propagate': True,
         },
     }
+}
+
+REST_FRAMEWORK = {
+    # Use hyperlinked styles by default.
+    # Only used if the `serializer_class` attribute is not set on a view.
+    'DEFAULT_MODEL_SERIALIZER_CLASS':
+        'rest_framework.serializers.HyperlinkedModelSerializer',
+
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
 }
--- wsgi/openshift/urls.py
+++ wsgi/openshift/urls.py
@@ -3,8 +3,27 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import RedirectView
+from rest_framework import viewsets, routers
+from schema_kernel.models import KernelVersion, PCIModule, PCIAliases
 
 admin.autodiscover()
+
+# ViewSets define the view behavior.
+class KernelVersionViewSet(viewsets.ModelViewSet):
+    model = KernelVersion
+
+class PCIModuleViewSet(viewsets.ModelViewSet):
+    model = PCIModule
+
+class PCIAliasesViewSet(viewsets.ModelViewSet):
+    model = PCIAliases
+
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'kernelversion', KernelVersionViewSet)
+router.register(r'pcimodule', PCIModuleViewSet)
+router.register(r'pcialiases', PCIAliasesViewSet)
 
 urlpatterns = patterns('',
     url(r'^$', 'views.home', name='home'),
@@ -18,4 +37,6 @@ urlpatterns = patterns('',
     # Upload stuff
     (r'^upload/', include('upload_kernel.urls')),
     (r'^$', RedirectView.as_view(url='/upload/list/')), # Just for ease of use.
+    url(r'^api/', include(router.urls)),
+    (r'^api/', include('rest_framework.urls', namespace='rest_framework')),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
