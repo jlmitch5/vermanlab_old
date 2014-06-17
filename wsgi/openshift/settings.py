531ca4d015b2f5ad64e643a63a03cd48c5f0af35
John Mitchell <jmitchel@redhat.com>
2014-06-17 16:43:53 -0400

    Cleaning up dir structure

--- wsgi/openshift/settings.py
+++ wsgi/openshift/settings.py
@@ -175,6 +175,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'bootstrap3',
     'upload_kernel',
     'schema_kernel',
 )
