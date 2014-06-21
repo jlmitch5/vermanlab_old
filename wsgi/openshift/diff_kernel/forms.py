4964b291075d268e2807ce4868133d0d567bbed4
John Mitchell <jmitchel@redhat.com>
2014-06-21 17:46:15 -0400

    Getting the diff form working correctly have the modules loading into a list

--- wsgi/openshift/diff_kernel/forms.py
+++ wsgi/openshift/diff_kernel/forms.py
@@ -6,7 +6,8 @@ class KernelDiffForm(forms.Form):
     
     def __init__(self,*args,**kwargs):
         kernel_list = kwargs.pop('kv_list')
+        # TODO: fix first part of tuple
         kernel_tuples = zip(kernel_list, kernel_list)
         super(KernelDiffForm, self).__init__(*args,**kwargs)
-        self.fields['kernel_one'] = forms.ChoiceField(label="kernel_one", choices=kernel_tuples)
-        self.fields['kernel_two'] = forms.ChoiceField(label="kernel_two", choices=kernel_tuples)
+        self.fields['kernel_one'] = forms.ChoiceField(label="kernel_one", choices=kernel_list)
+        self.fields['kernel_two'] = forms.ChoiceField(label="kernel_two", choices=kernel_list)
--- wsgi/openshift/diff_kernel/views.py
+++ wsgi/openshift/diff_kernel/views.py
@@ -8,11 +8,20 @@ from diff_kernel.forms import KernelDiffForm
 def diff(request):
     r = requests.get('http://localhost:8000/api/kv_list/', params=request.GET)
     kvs = json.loads(r.text)
+    kvs= list((e[str('name')] for e in kvs))
+    kvs = zip(kvs, kvs)
     
     if request.method == 'POST':
-        form = KernelDiffForm(request.POST)
+        form = KernelDiffForm(request.POST, kv_list = kvs)
         if form.is_valid():
-            return HttpResponseRedirect('diff_kernel.views.list')
+            # process the form data
+            k1 = form.cleaned_data['kernel_one']
+            k2 = form.cleaned_data['kernel_two']
+            kernels_to_diff_link = 'http://jmitchelnode.usersys.redhat.com:8000/api/pci_mod_list_intersection/' + k1 + '/' + k2
+            r2 = requests.get(kernels_to_diff_link, params=request.GET)
+            mods = json.loads(r2.text)
+            print mods
+
     else:
         form = KernelDiffForm(kv_list = kvs)
 
