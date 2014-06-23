4dfaa31c69d03bed7f508cd0764c5bfbc006bae0
John Mitchell <jlmitch5@ncsu.edu>
2014-06-23 14:26:43 -0400

    Adding code to make openshift work

--- wsgi/openshift/diff_kernel/views.py
+++ wsgi/openshift/diff_kernel/views.py
@@ -1,12 +1,15 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-import requests, json
+import requests, json, settings
 
 from diff_kernel.forms import KernelDiffForm
 
 # Create your views here.
 def diff(request):
-    r = requests.get('http://localhost:8000/api/kv_list/', params=request.GET)
+    if settings.ON_OPENSHIFT:
+        r = requests.get('http://python-jmitchel.rhcloud.com/api/kv_list/', params=request.GET)
+    else:       
+        r = requests.get('http://localhost:8000/api/kv_list/', params=request.GET)
     kvs = json.loads(r.text)
     kvs= list((e[str('name')] for e in kvs))
     kvs = zip(kvs, kvs)
@@ -21,8 +24,12 @@ def diff(request):
             k1 = form.cleaned_data['kernel_one']
             k2 = form.cleaned_data['kernel_two']
 
-            kernel_id_1_link = 'http://localhost:8000/api/kv_id/' + k1
-            kernel_id_2_link = 'http://localhost:8000/api/kv_id/' + k2
+            if settings.ON_OPENSHIFT:
+                kernel_id_1_link = 'http://python-jmitchel.rhcloud.com/api/kv_id/' + k1
+                kernel_id_2_link = 'http://python-jmitchel.rhcloud.com/api/kv_id/' + k2
+            else:
+                kernel_id_1_link = 'http://localhost:8000/api/kv_id/' + k1
+                kernel_id_2_link = 'http://localhost:8000/api/kv_id/' + k2
 
             kv1_r = requests.get(kernel_id_1_link, params=request.GET)
             kv2_r = requests.get(kernel_id_2_link, params=request.GET)
@@ -31,7 +38,10 @@ def diff(request):
             kv1 = kv1['id']
             kv2 = kv2['id']
 
-            kernels_to_diff_link = 'http://localhost:8000/api/pci_mod_list_intersection/' + k1 + '/' + k2
+            if settings.ON_OPENSHIFT:
+                kernels_to_diff_link = 'http://python-jmitchel.rhcloud.com/api/pci_mod_list_intersection/' + k1 + '/' + k2
+            else:
+                kernels_to_diff_link = 'http://localhost:8000/api/pci_mod_list_intersection/' + k1 + '/' + k2
 
             r2 = requests.get(kernels_to_diff_link, params=request.GET)
 
@@ -41,7 +51,10 @@ def diff(request):
             list_names = []
 
             for mod in mods:
-                mod_pretty_link = 'http://localhost:8000/api/mod_pretty/' + str(mod['id'])
+                if settings.ON_OPENSHIFT:
+                    mod_pretty_link = 'http://python-jmitchel.rhcloud.com/api/mod_pretty/' + str(mod['id'])
+                else:
+                    mod_pretty_link = 'http://localhost:8000/api/mod_pretty/' + str(mod['id'])
                 req_mod_pretty = requests.get(mod_pretty_link, params=request.GET)
                 mod_pretty = json.loads(req_mod_pretty.text)
 
