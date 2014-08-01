db4e3f28842db1be2e8b3de7fdc1ffd6a307952d
John Mitchell <jmitchel@redhat.com>
2014-08-01 09:24:48 -0400

    trying to update to Django 1.7 to handle migrations

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author_email='jmitchel@example.com',
     url='http://www.python.org/sigs/distutils-sig/',
     install_requires=[
-	'Django>=1.6,<1.7',
+	'Django>=1.7c2,<1.8',
 	'django-widget-tweaks>=1.3,<1.4',
     'djangorestframework>=2.3,<2.4',
     'requests>=2.3,<2.4',
